1b0191486de133551e2105d2829228a5fd022566
FireStoneY <59555629+FireStoneY@users.noreply.github.com>
2021-05-08 16:07:13 +0300

    main changes

---
+++ Source/Calculator/ElementNumber.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Element.h"
+#include "ElementNumber.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CALCULATOR_API AElementNumber : public AElement
+{
+	GENERATED_BODY()
+	
+};
