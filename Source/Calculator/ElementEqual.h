5454f645b9bf8a00583e0bdc5991f1d43b156fcc
FireStoneY <59555629+FireStoneY@users.noreply.github.com>
2021-05-10 19:44:46 +0300

    Made logic

---
+++ Source/Calculator/ElementEqual.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Element.h"
+#include "ElementEqual.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CALCULATOR_API AElementEqual : public AElement
+{
+	GENERATED_BODY()
+	
+};
