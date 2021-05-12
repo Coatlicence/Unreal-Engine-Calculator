eb77d206b029d8370c245387c088d2aacdae39ea
FireStoneY <59555629+FireStoneY@users.noreply.github.com>
2021-05-12 18:01:51 +0300

    final 0.1

---
+++ Source/Calculator/ElementEraser.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Element.h"
+#include "ElementEraser.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CALCULATOR_API AElementEraser : public AElement
+{
+	GENERATED_BODY()
+	
+};
